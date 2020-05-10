0f9f5435084cae13613e8ddef2ee29c3e27e8c8f
Julie Rymer <rymerjulie.pro@gmail.com>
2020-05-10 16:51:20 +0200

    [feat] file properties better visual presentation

---
+++ src/components/DirectoryPresenter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ul v-for="dir in dirProperties" v-bind:key="dir.name">
+            <li><strong>{{dir.name}}:</strong>
+                <template v-if="dir.type === 'DIR'">
+                    <DirectoryPresenter :dirProperties="dir.content"/>
+                </template>
+                <template v-else>
+                    <ul v-for="(dir_prop, prop_name) in dir.props" v-bind:key="prop_name">
+                        <li>{{prop_name}} : {{dir_prop}}</li>
+                    </ul>
+                </template>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'DirectoryPresenter',
+    props: {
+        dirProperties: Array
+    },
+
+    mounted: function () {
+        // `this` points to the vm instance
+        console.log(this.dirProperties)
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/components/ServerFilesPresenter.vue
+++ src/components/ServerFilesPresenter.vue
@@ -3,47 +3,59 @@
         <template v-if="loading">
             <p>Loading...</p>
         </template>
-        <template v-else-if="dirProperties == null">
+        <template v-else>
             <!--    TODO use proper vue form validation instead -->
             <div>
-                <label>
-                    Directory path:
-                    <input type="text" v-model="rootDir">
-                </label>
-                <label>
-                    Max recursivity level:
-                    <input v-model.number="maxLevel" type="number" min="0" oninput="validity.valid || (value='');">
-                </label>
-                <button @click="selectNewDir">Explore this directory</button>
+                <div>
+                    <label>
+                        Directory path:
+                        <input type="text" v-model="newRootDir">
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Max recursivity level:
+                        <input v-model.number="newMaxLevel" type="number" min="0" oninput="validity.valid || (value='');">
+                    </label>
+                </div>
+                <div>
+                    <button @click="selectNewDir">Explore this directory</button>
+                </div>
             </div>
-        </template>
-        <template v-else>
-            {{ dirProperties }}
+            <template v-if="dirProperties != null">
+                <p><strong>Properties for directory "{{ rootDir }}" at max level {{ maxLevel }}</strong></p>
+
+                <DirectoryPresenter :dirProperties="dirProperties"/>
+            </template>
         </template>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import DirectoryPresenter from "@/components/DirectoryPresenter";
 
 export default {
   name: 'ServerFilesPresenter',
+  components: {
+    DirectoryPresenter
+  },
   data() {
     return {
       // use store to set initial values
-      rootDir: this.$store.state.rootDir,
-      maxLevel: this.$store.state.maxLevel
+      newRootDir: this.$store.state.rootDir,
+      newMaxLevel: this.$store.state.maxLevel
     }
   },
   computed: {
-    ...mapState(['loading', 'dirProperties'])
+    ...mapState(['loading', 'rootDir', 'maxLevel', 'dirProperties'])
   },
   methods: {
     selectNewDir() {
       this.$store.dispatch({
         type: 'selectNewDir',
-        rootDir: this.rootDir,
-        maxLevel: this.maxLevel
+        rootDir: this.newRootDir,
+        maxLevel: this.newMaxLevel
       });
     }
   }
@@ -52,18 +64,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/*h3 {*/
-/*  margin: 40px 0 0;*/
-/*}*/
-/*ul {*/
-/*  list-style-type: none;*/
-/*  padding: 0;*/
-/*}*/
-/*li {*/
-/*  display: inline-block;*/
-/*  margin: 0 10px;*/
-/*}*/
-/*a {*/
-/*  color: #42b983;*/
-/*}*/
+input {
+    margin-bottom: 10px;
+}
 </style>
