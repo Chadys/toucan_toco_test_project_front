b7dc23d5fcbbd4d60ba177afa3bb016cd5358cdd
Julie Rymer <rymerjulie.pro@gmail.com>
2020-05-10 15:18:42 +0200

    [feat] adapt store and files presenter to directory exploration feature

--- src/components/ServerFilesPresenter.vue
+++ src/components/ServerFilesPresenter.vue
@@ -1,17 +1,50 @@
 <template>
-  <div>
-    <button @click="increment">inc</button>
-  </div>
+    <div>
+        <template v-if="loading">
+            <p>Loading...</p>
+        </template>
+        <template v-else-if="dirProperties == null">
+            <!--    TODO use proper vue form validation instead -->
+            <div>
+                <label>
+                    Directory path:
+                    <input type="text" v-model="rootDir">
+                </label>
+                <label>
+                    Max recursivity level:
+                    <input v-model.number="maxLevel" type="number" min="0" oninput="validity.valid || (value='');">
+                </label>
+                <button @click="selectNewDir">Explore this directory</button>
+            </div>
+        </template>
+        <template v-else>
+            {{ dirProperties }}
+        </template>
+    </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'ServerFilesPresenter',
-  props: {},
+  data() {
+    return {
+      // use store to set initial values
+      rootDir: this.$store.state.rootDir,
+      maxLevel: this.$store.state.maxLevel
+    }
+  },
+  computed: {
+    ...mapState(['loading', 'dirProperties'])
+  },
   methods: {
-    increment() {
-      this.$store.commit('increment')
-      console.log(this.$store.state.count)
+    selectNewDir() {
+      this.$store.dispatch({
+        type: 'selectNewDir',
+        rootDir: this.rootDir,
+        maxLevel: this.maxLevel
+      });
     }
   }
 }
--- src/state/store.js
+++ src/state/store.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state: {
-        count: 0
+        loading: false,
+        rootDir: null,
+        maxLevel: 1,
+        dirProperties: null
+    },
+    actions: {
+        // use this function instead of _selectNewDir directly to keep dirProperties in sync
+        selectNewDir ({ commit, dispatch }, payload) {
+            commit('_selectNewDir', payload);
+            dispatch('_updateProperties');
+        },
+        async _updateProperties ({ commit, state }) {
+            commit('_load');
+            // TODO replace mock with async call to webservice to get new dirProperties
+            await new Promise(resolve => setTimeout(resolve, state.maxLevel*1000));
+            commit('_setProperties', { dirProperties: null });
+        }
     },
     mutations: {
-        increment (state) {
-            state.count++
+        _load (state) {
+            state.loading = true;
+        },
+        _selectNewDir (state, { rootDir, maxLevel }) {
+            state.rootDir = rootDir;
+            state.maxLevel = maxLevel;
+        },
+        _setProperties (state, { dirProperties }) {
+            state.dirProperties = dirProperties;
+            state.loading = false;
         }
     }
 })
